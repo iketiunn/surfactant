f2d2f2c7a7e152b73193f37081761b18831e15ef
ike <developer@ikewat.com>
2019-11-29 11:53:55 +0800

    Handle error message in cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,12 @@
+#!/usr/bin/env node
+
 import { genAsyncMethodInterface } from "./";
 
 Promise.resolve()
   .then(() => process.argv.slice(2)[0])
   .then(genAsyncMethodInterface)
-  .then(console.log);
+  .then(console.log)
+  .catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
